9a220a4c93e0c47ec40eb2451108461168c6273d
uraniumreza <reza.uranium@gmail.com>
2020-04-14 13:57:37 +0600

    Remove falsy tests for getEnglishNumber function

--- src/__tests__/Ongko.test.ts
+++ src/__tests__/Ongko.test.ts
@@ -35,39 +35,3 @@ describe('GetBanglaNumber', () => {
     expect(() => Ongko.getBanglaNumber('12.34.54')).toThrow();
   });
 });
-
-describe('GetEnglishNumber', () => {
-  test('when passes 1287125', () => {
-    expect(Ongko.getEnglishNumber('১২৮৭১২৫')).toBe('1287125');
-  });
-  test("when passes '1287125'", () => {
-    expect(Ongko.getEnglishNumber('১২৮৭১২৫')).toBe('1287125');
-  });
-  test('when passes 1287.125', () => {
-    expect(Ongko.getEnglishNumber('১২৮৭.১২৫')).toBe('1287.125');
-  });
-  test("when passes '1287.125'", () => {
-    expect(Ongko.getEnglishNumber('১২৮৭.২৫')).toBe('1287.25');
-  });
-  test('when passes 0.125', () => {
-    expect(Ongko.getEnglishNumber('০.১২৫')).toBe('0.125');
-  });
-  test('when passes -0.125', () => {
-    expect(Ongko.getEnglishNumber('-০.১২৫')).toBe('-0.125');
-  });
-  test("when passes '-.125'", () => {
-    expect(Ongko.getEnglishNumber('-.১২৫')).toBe('-.125');
-  });
-  test('when passes -5.125', () => {
-    expect(Ongko.getEnglishNumber('-৫.১২৫')).toBe('-5.125');
-  });
-  test("when passes '12,87,125'", () => {
-    expect(() => Ongko.getEnglishNumber('১২,৮৭,১২৫')).toBe('12,87,125');
-  });
-  test("when passes '1234e54'", () => {
-    expect(() => Ongko.getEnglishNumber('১২.৩৪ই৫৪')).toThrow();
-  });
-  test('when passes 12.34.54', () => {
-    expect(() => Ongko.getEnglishNumber('১২.৩৪.৫৪')).toThrow('12.34.54');
-  });
-});
